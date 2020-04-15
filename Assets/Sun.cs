adce08818f9d208a3b7dcd2cb4f1196010bb7186
Depthperception2166 <jwm2166@gmail.com>
2020-04-15 12:43:50 -0500

    Map setup

---
+++ Assets/Sun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+    public Transform sun;
+    public float distance = 20f;
+    public float speed = 100f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            var newPosition = sun.position + new Vector3(0, distance, 0);
+            StartCoroutine(MoveToPosition(sun, newPosition, 4f));
+        }
+    }
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+        var currentPos = transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+    }
+}
